0502d158e0f572b008c14e86832201b82c64bf1b
MemeFlavor <Memeflavor@users.noreply.github.com>
2026-06-05 00:42:12 +0800

    it works :face_holding_back_tears:

--- javascript/bloglists.js
+++ javascript/bloglists.js
@@ -4,31 +4,31 @@ function toUpperFirstCase(str) {
      return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-const albumlistBody = document.querySelector('.blogs__bloglist tbody')
-const albumlistSection = document.querySelector('.blogs__bloglist-section');
-const albumlistContent = document.querySelector('.blogs__bloglist-content');
-albumlistSection.remove();
-albumlistContent.remove();
+const bloglistBody = document.querySelector('.blogs__bloglist tbody')
+const bloglistSection = document.querySelector('.blogs__bloglist-section');
+const bloglistContent = document.querySelector('.blogs__bloglist-content');
+bloglistSection.remove();
+bloglistContent.remove();
 
 for (const [key, value] of Object.entries(featured)) {
      if (Object.keys(value).length === 0) break; // No empty content, there's nothing special here
 
-     const albumlistSectionClone = albumlistSection.cloneNode(true);
-     albumlistSectionClone.querySelector('.blogs__bloglist-section-grp').textContent = toUpperFirstCase(key)
-     albumlistBody.appendChild(albumlistSectionClone)
+     const bloglistSectionClone = bloglistSection.cloneNode(true);
+     bloglistSectionClone.querySelector('.blogs__bloglist-section-grp').textContent = toUpperFirstCase(key)
+     bloglistBody.appendChild(bloglistSectionClone)
 
      let elementIndices = 0;
      for (const element of Object.values(value)) {
           elementIndices += 1; if (elementIndices > 3) break;
-          const albumlistContentClone = albumlistContent.cloneNode(true);
+          const bloglistContentClone = bloglistContent.cloneNode(true);
           
-          albumlistContentClone.querySelector('.blogs__bloglist-link').setAttribute('href', element.title.link);
-          albumlistContentClone.querySelector('.blogs__bloglist-title').textContent = element.title.content;
+          bloglistContentClone.querySelector('.blogs__bloglist-link').setAttribute('href', element.title.link);
+          bloglistContentClone.querySelector('.blogs__bloglist-title').textContent = element.title.content;
 
-          albumlistContentClone.querySelector('.blogs__bloglist-type').textContent = element.type;
-          albumlistContentClone.querySelector('.blogs__bloglist-description').textContent = element.description;
-          albumlistContentClone.querySelector('.blogs__bloglist-time').textContent = element.time;
-          albumlistContentClone.querySelector('.blogs__bloglist-time').setAttribute('datetime', element.time)
-          albumlistBody.appendChild(albumlistContentClone);
+          bloglistContentClone.querySelector('.blogs__bloglist-type').textContent = element.type;
+          bloglistContentClone.querySelector('.blogs__bloglist-description').textContent = element.description;
+          bloglistContentClone.querySelector('.blogs__bloglist-time').textContent = element.time;
+          bloglistContentClone.querySelector('.blogs__bloglist-time').setAttribute('datetime', element.time)
+          bloglistBody.appendChild(bloglistContentClone);
      }
 }

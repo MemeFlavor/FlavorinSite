9bf4569216b6022d001caab0fb015d7912434af3
MemeFlavor <Memeflavor@users.noreply.github.com>
2026-06-14 16:06:41 +0800

    logs

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,6 +1,6 @@
-async function getData() {
+async function trag(file) {
      try {
-          const response = await fetch('changelog.txt');
+          const response = await fetch(file);
           if (!response.ok) { // Check if the request was successful
                throw new Error('Network response was not ok');
           }
@@ -10,43 +10,71 @@ async function getData() {
      }
 }
 
-let dod = await getData()
+const die = await trag('changelog.txt')
+const cie = await trag('help.txt')
 
-const huy = dod.match(/commit.+/g)
+const ert = document.querySelector('.updatelogs__terminal')
+
+function hjkl() {
+     let divs = document.createElement('div')
+     divs.insertAdjacentHTML('beforeend', die
+          .replace(/host\.name/g, window.location.hostname)
+          .replace(/(commit.+)/, `${die.match(/commit.+/g)[0]} (<color-head>HEAD -></color-head> <color-main>main</color-main>)`)
+          .replace(/(commit.+)/g, '<color-commit>$1</color-commit>') + '\n\n'
+     )
+     ert.appendChild(divs)
+}
+
+hjkl()
+
+document.querySelector('.updatelogs__input').addEventListener('keydown', (event) => {
+     if (event.key === 'Enter') {
+          event.preventDefault();
+
+          if (event.target.value.match('log')) {
+               hjkl()
+
+               const iop = document.querySelector('.updatelogs__console')
+
+               const we = document.querySelectorAll('color-head')[1]
+               iop.scrollTop = iop.scrollHeight - iop.clientHeight;
+          }
+     }
+})   
+
+/* let wer = await (async () => {
+     try {
+          const response = await fetch('changelog.txt');
+          if (!response.ok) { // Check if the request was successful
+               throw new Error('Network response was not ok');
+          }
+          return await new Response(response.body).text();
+     } catch (error) {
+          console.error('Error fetching data:', error);
+     }
+})()
+
+const huy = wer.match(/commit.+/g)
 const yu = huy.map((value) => {
                return `<span style="color: #ffb300">${value}</span>`
           })
           .forEach((value, index) => {
-               dod = dod.replace(huy[index], value)
+               wer = wer.replace(huy[index], value)
           })
+const iop = wer.split('\n\n').slice(0, 2).join('\n\n')
 
-const iop = dod.split('\n\n').slice(0, 2).join('\n\n')
+const werty = document.querySelector('.updatelogs__terminal')
+const iuop = document.querySelector('.updatelogs__input')
 
-document.querySelector('.updatelogs__terminal').innerHTML = dod
-document.querySelector('.updatelogs__terminal span').innerHTML += ' (<span style="color: #51cbd3; font-weight: bolder">HEAD -></span> <span style="color: #43cd5f; font-weight: bolder">main</span>)'
+const g = wer.replace(/host\.name/g, window.location.hostname)
+werty.innerHTML += '<div>' + g + '\n\n' + '</div>'
 
-//const container = document.querySelector('.updatelogs__console')
-//container.scrollTop = container.scrollHeight - container.clientHeight;
+const grtyt = document.querySelector('.updatelogs__terminal div span')
+const hyrts = ' (<span style="color: #51cbd3; font-weight: bolder">HEAD -></span> <span style="color: #43cd5f; font-weight: bolder">main</span>)'
+grtyt.innerHTML += hyrts
 
-//console.log(dod)
-
-/* const yu = data.match(/commit.+/g).map(value => `<span style="color: #ffb300">${value}</span>`)
-          for (const index in yu) {
-               data = data.replace(data.match(/commit.+/g)[index], yu[index])
-          }
-          
-          document.querySelector('.updatelogs__terminal').innerHTML = data
-          document.querySelector('.updatelogs__terminal span').innerHTML += ' (<span style="color: #51cbd3; font-weight: bolder">HEAD -></span> <span style="color: #43cd5f; font-weight: bolder">main</span>)'
-
-          const container = document.querySelector('.updatelogs__console')
-          container.scrollTop = container.scrollHeight - container.clientHeight;
-
-          const we = document.querySelector('.updatelogs__terminal').textContent.split('\n\n') */
-
-
-
-//document.querySelector('.updatelogs__terminal').insertAdjacentHTML('beforeend', dod);
-
-
-//console.log(dod)
-
+iuop.addEventListener('keydown', (event) => {
+     if (event.key === 'Enter') {
+          event.preventDefault();
+     } 
+}) */

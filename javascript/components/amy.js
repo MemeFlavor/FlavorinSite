c3a2cbf26497f8eea72143bab646fa8c301d647e
MemeFlavor <Memeflavor@users.noreply.github.com>
2026-06-18 22:17:49 +0800

    fixed a critical bug for older browsers

--- javascript/components/amy.js
+++ javascript/components/amy.js
@@ -3,6 +3,7 @@ import { getRandomInt } from "../modules/shortcuts.js";
 const LIMIT = 60
 const STEPS = 10
 const amy = document.querySelector('.music__album-amy')
+const dlg = document.querySelector('.music__album-dialogue')
 
 let position = 0
 const dancingMovement = setInterval(() => {
@@ -12,6 +13,8 @@ const dancingMovement = setInterval(() => {
      }
 
      if (position >= -LIMIT && position <= LIMIT) {
+          if (CSS.supports('position-anchor: --test')) 
+               dlg.style.transform = `translateX(${position}px)`;
           amy.style.transform = `translateX(${position}px)`
      } else if (position <= -LIMIT || position >= LIMIT) {
           position -= positionRancrement * STEPS

53f7371e411a19e761ec90f3562db2533992e799
MemeFlavor <Memeflavor@users.noreply.github.com>
2026-08-13 22:00:57 +0800

    manual design update

--- javascript/blogs.js
+++ javascript/blogs.js
@@ -1,4 +1,4 @@
-import { getRandomInt } from "./modules/shortcuts.js";
+/* import { getRandomInt } from "./modules/shortcuts.js";
 
 const bannerAccr = document.querySelectorAll('.manual__banner-accuracy')
 const bannerAccrCrement = document.querySelectorAll('.manual__banner-accuracy accr-crement')
@@ -12,4 +12,4 @@ for (let accuracyIndex = 0; accuracyIndex < 3; accuracyIndex++) {
      accuracyLines.style.color = randomize === 1 ? 'hsl(89, 96%, 63%)' : 'hsl(357, 94%, 60%)'
      accuracyCrements.innerHTML = randomize === 1 ? '&#9650;' : '&#9660;'
      accuracyNumbers.innerHTML = `${getRandomInt(0, 35)}.${getRandomInt(0, 9)}%`.padStart(5, '0')
-}
+} */

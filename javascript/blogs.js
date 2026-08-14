23f9870e166ce621c400906d1ebd2067b3e1b393
MemeFlavor <Memeflavor@users.noreply.github.com>
2026-08-14 14:30:30 +0800

    reduce motion support

--- javascript/blogs.js
+++ javascript/blogs.js
@@ -1,20 +1,26 @@
-const motionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+const prefersReduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
 
-function yesy_motion() {
-     //alert('has motion')
+const windowScreenSaverWrapper = document.querySelector('.windows__screensaver')
+const windowScreenSaverBody = document.querySelector('.windows__screensaver marquee')
+const wackyworkbenchSonic = document.querySelector('.wacky-workbench__sonic img')
+
+function reduce_motion() {
+     windowScreenSaverWrapper.setAttribute('scrollamount', 2)
+     windowScreenSaverBody.setAttribute('scrollamount', 2)
+
+     wackyworkbenchSonic.style.display = 'none'
 }
 
-function no_motion() {
-     //alert('no motion')
+function default_motion() {
+     windowScreenSaverWrapper.setAttribute('scrollamount', 4)
+     windowScreenSaverBody.setAttribute('scrollamount', 4)
+
+     wackyworkbenchSonic.style.display = 'block'
 }
 
-function is_motion(asdr) {
-     if (asdr.matches == true) {
-          yesy_motion()
-     } else {
-          no_motion()
-     }
+function has_motion(event) {
+     if (event.matches == true) { reduce_motion() } else { default_motion() }
 }
 
-is_motion(motionQuery)
-motionQuery.addEventListener('change', is_motion);
+has_motion(prefersReduceMotion)
+prefersReduceMotion.addEventListener('change', has_motion);
